c92ad1b3dfeb5473649b89bc0411e55d36cb7a1b
luanjin <luanjin861203@sina.com>
2017-09-15 18:49:17 +0800

    修改

--- client/modules/exer/mixins.jsx
+++
@@ -1,9 +0,0 @@
-const Mixinlog = {
-    
-    componentWillMount() {
-        console.log('componentWillMount  in   Mixin')  
-    },
-    
-    log() { console.log(12345677890) }
-};
-export default Mixinlog;
